e6f185372e6835b36268f6f8d5681f9a23911806
zfonemore <zfonemore@foxmail.com>
2022-05-07 23:23:58 +1000

    add more frames one video in training

--- mask2former_video/config.py
+++ mask2former_video/config.py
@@ -6,7 +6,9 @@ from detectron2.config import CfgNode as CN
 def add_maskformer2_video_config(cfg):
     # video data
     # DataLoader
-    cfg.INPUT.SAMPLING_FRAME_NUM = 6
+    cfg.INPUT.SAMPLING_FRAME_NUM = 10
     cfg.INPUT.SAMPLING_FRAME_RANGE = 20
     cfg.INPUT.SAMPLING_FRAME_SHUFFLE = False
     cfg.INPUT.AUGMENTATIONS = [] # "brightness", "contrast", "saturation", "rotation"
+    cfg.INPUT.TRAIN_CLIP_LEN = 2
+    cfg.INPUT.EVAL_CLIP_LEN = 5
